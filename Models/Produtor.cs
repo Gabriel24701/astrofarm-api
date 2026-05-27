0ac78e2ac7b0f44bbec90aa8ff4d54a72ec82eba
GabrielSilva <gabrielbebesilva247@gmail.com>
2026-05-26 22:32:12 -0300

    feat: adicionando entidades Produtor e Propriedade

---
+++ Models/Produtor.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AstroFarm.Api.Models
+{
+    [Table("PRODUTOR")]
+    public class Produtor
+    {
+        [Key]
+        [Column("ID_PRODUTOR")]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("NOME")]
+        public string Nome { get; set; } = string.Empty;
+
+        [Required]
+        [Column("CPF")]
+        public string Cpf { get; set; } = string.Empty;
+
+        [Column("EMAIL")]
+        public string? Email { get; set; }
+
+        [Column("TELEFONE")]
+        public string? Telefone { get; set; }
+
+        [Required]
+        [Column("ESTADO")]
+        public string Estado { get; set; } = string.Empty;
+
+        [Required]
+        [Column("CIDADE")]
+        public string Cidade { get; set; } = string.Empty;
+
+        [Column("DT_CADASTRO")]
+        public DateTime DtCadastro { get; set; } = DateTime.Now;
+        
+        public ICollection<Propriedade> Propriedades { get; set; } = new List<Propriedade>();
+    }
+}
---
+++ Models/Propiedade.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AstroFarm.Api.Models
+{
+    [Table("PROPRIEDADE")]
+    public class Propriedade
+    {
+        [Key]
+        [Column("ID_PROPRIEDADE")]
+        public int Id { get; set; }
+
+        [Column("ID_PRODUTOR")]
+        public int ProdutorId { get; set; }
+
+        [Required]
+        [Column("NOME_FAZENDA")]
+        public string NomeFazenda { get; set; } = string.Empty;
+
+        [Required]
+        [Column("AREA_HECTARES")]
+        public double AreaHectares { get; set; }
+
+        [Column("LATITUDE")]
+        public double? Latitude { get; set; }
+
+        [Column("LONGITUDE")]
+        public double? Longitude { get; set; }
+
+        [Required]
+        [Column("ESTADO")]
+        public string Estado { get; set; } = string.Empty;
+
+        [Required]
+        [Column("MUNICIPIO")]
+        public string Municipio { get; set; } = string.Empty;
+
+        [Column("DT_REGISTRO")]
+        public DateTime DtRegistro { get; set; } = DateTime.Now;
+
+        [ForeignKey("ProdutorId")]
+        public Produtor? Produtor { get; set; }
+    }
+}
