841f2121f42b4e0f8fcb3a193a2c4cb26a7059ea
GabrielSilva <gabrielbebesilva247@gmail.com>
2026-05-26 22:34:44 -0300

    feat: configurando AppDbContext e injeção de dependência do Oracle

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using AstroFarm.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AstroFarm.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Produtor> Produtores { get; set; }
+        public DbSet<Propriedade> Propriedades { get; set; }
+        // Adicione as linhas abaixo
+        public DbSet<LeituraSatelital> Leituras { get; set; }
+        public DbSet<Alerta> Alertas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Propriedade>()
+                .HasOne(p => p.Produtor)
+                .WithMany(p => p.Propriedades)
+                .HasForeignKey(p => p.ProdutorId);
+            
+        }
+    }
+}
