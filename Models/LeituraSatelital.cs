67240c92a66b168b12149bd061e6d28d2502993b
GabrielSilva <gabrielbebesilva247@gmail.com>
2026-05-26 22:34:06 -0300

    feat: adicionando entidades Alerta e Leitura Satelital.

---
+++ Models/LeituraSatelital.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AstroFarm.Api.Models
+{
+    [Table("LEITURA_SATELITAL")]
+    public class LeituraSatelital
+    {
+        [Key]
+        [Column("ID_LEITURA")]
+        public int Id { get; set; }
+
+        [Column("ID_PROPRIEDADE")]
+        public int PropriedadeId { get; set; }
+
+        [Required]
+        [Column("DT_LEITURA")]
+        public DateTime DtLeitura { get; set; }
+
+        [Column("NDVI")]
+        public double? Ndvi { get; set; }
+
+        [Column("TEMPERATURA")]
+        public double? Temperatura { get; set; }
+
+        [Column("UMIDADE")]
+        public double? Umidade { get; set; }
+
+        [Column("PRECIPITACAO")]
+        public double? Precipitacao { get; set; }
+
+        [Column("FONTE_SATELITE")]
+        public string? FonteSatelite { get; set; }
+
+        [ForeignKey("PropriedadeId")]
+        public Propriedade? Propriedade { get; set; }
+    }
+}
